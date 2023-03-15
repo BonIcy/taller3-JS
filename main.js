0908ad6750a61dcd26ad19572826939d38131aad
JesusEduardoMartinezReyesCAMPUS1 <mrlgsus22@gmail.com>
2023-03-15 09:26:53 -0500

    feat: :sparkles: taller3.1
    se inicia la base del taller

---
+++ main.js
@@ -0,0 +1,32 @@
+let myFormularioCampus = document.querySelector("#myFormularioCampus");
+let myFormularioPersonas = document.querySelector("#myFormularioPersonas");
+let campus = {};
+
+myFormularioCampus.addEventListener("submit", (e)=>{
+    e.preventDefault();
+    let data = Object.fromEntries(new FormData(e.target))
+    campus[`${data.nombreSede}`] = {Camper: [], Trainers: []};
+    listaSedes();
+    myFormularioCampus.reset();
+})
+
+let listaSedes = ()=>{
+    let opciones = document.querySelector("[name='sede']");
+    opciones.innerHTML = null;
+    for (let [val, id] of Object.entries(campus)) {
+        opciones.insertAdjacentHTML("beforeend", `
+            <option value="${val}">${val}</option>
+        `);
+    }
+}
+
+myFormularioPersonas.addEventListener("submit", (e)=>{
+    e.preventDefault();
+    let data = Object.fromEntries(new FormData(e.target));
+    console.log(data);
+    let sede = data.sede;
+    delete data.sede;
+    campus[`${sede}`]["Camper"].unshift(data);
+    console.log(campus);
+    myFormularioPersonas.reset();
+})
