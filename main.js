351c69897c76f673f762ec8170ba332dba841b83
JesusEduardoMartinezReyesCAMPUS1 <mrlgsus22@gmail.com>
2023-03-16 09:26:38 -0500

    feat: :sparkles: Html completo
    se completo el html y se le dio funcionalidad a la seccion de campers con javascript, no a trainers aun

--- main.js
+++ main.js
@@ -1,5 +1,19 @@
+/* 1. Construir un objeto literal "campus" que gestione
+la info(PROPIEDADES) de Campus, trainers, campers, niveles,
+tecnologías, teams y roadMap
+1.1. De campus administrar los datos de contacto de las sedes en
+Bucaramanga, Bogotá, Medellín y México
+1.2. De los trainers y campers, su nombre, sus teléfonos, teams
+(horarios de las teams=> día, hora y salones (nro y piso), y el
+email, y de los campers también horarios de inglés y ser.
+1.3. De los campers, también gestionar su nivel actual, como su
+barrio y medio de transporte
+1.4. De los niveles, su pre requisito, a que tecnología pertenece, si
+es electiva u obligatoria
+1.5. De la roadmap , Nro de créditos, año, Nro de asignaturas */
 let myFormularioCampus = document.querySelector("#myFormularioCampus");
-let myFormularioPersonas = document.querySelector("#myFormularioPersonas");
+let myFormularioCamper = document.querySelector("#myFormularioCamper");
+let myFormularioTrainer = document.querySelector("#myFormularioTrainer");
 let campus = {};
 
 myFormularioCampus.addEventListener("submit", (e)=>{
@@ -20,7 +34,7 @@ let listaSedes = ()=>{
     }
 }
 
-myFormularioPersonas.addEventListener("submit", (e)=>{
+myFormularioCamper.addEventListener("submit", (e)=>{
     e.preventDefault();
     let data = Object.fromEntries(new FormData(e.target));
     console.log(data);
@@ -28,5 +42,16 @@ myFormularioPersonas.addEventListener("submit", (e)=>{
     delete data.sede;
     campus[`${sede}`]["Camper"].unshift(data);
     console.log(campus);
-    myFormularioPersonas.reset();
+    myFormularioCamper.reset();
+})
+
+myFormularioTrainer.addEventListener("submit", (e)=>{
+    e.preventDefault();
+    let data = Object.fromEntries(new FormData(e.target));
+    console.log(data);
+    let sede = data.sede;
+    delete data.sede;
+    campus[`${sede}`]["Camper"].unshift(data);
+    console.log(campus);
+    myFormularioTrainer.reset();
 })
